ecba5e57ff50b49289b4cc0725a555ff043f4ea8
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-25 23:38:03 +1100

    delete endpoint done

---
+++ src/API/ESkitNet.API/Products/Delete/Endpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace ESkitNet.API.Products.Delete;
+
+public record Response(bool IsSuccess);
+
+public class Endpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new Command(id));
+
+            var response = result.Adapt<Response>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteProduct")
+        .Produces<Response>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Product")
+        .WithDescription("Delete Product");
+    }
+}
---
+++ src/API/ESkitNet.API/Products/Delete/Handler.cs
@@ -0,0 +1,28 @@
+﻿namespace ESkitNet.API.Products.Delete;
+
+public record Command(Guid ProductId) : ICommand<Result>;
+
+public record Result(bool IsSuccess);
+
+public class Handler(StoreDbContext dbContext) : ICommandHandler<Command, Result>
+{
+    public async Task<Result> Handle(Command command, CancellationToken cancellationToken)
+    {
+        //Delete Order entity from command object
+        //save to database
+        //return result
+
+        var productId = ProductId.Of(command.ProductId);
+        var product = await dbContext.Products
+            .FindAsync([productId], cancellationToken: cancellationToken);
+
+        if (product is null)
+            throw new ProductNotFoundException(command.ProductId);
+
+        dbContext.Products.Remove(product);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new Result(true);
+    }
+}
