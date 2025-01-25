58cc7ecf26c0ea34babb42c2090251e9d161c3e9
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-25 23:33:03 +1100

    Added updated endpoint

---
+++ src/API/ESkitNet.API/Products/Update/Endpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace ESkitNet.API.Products.Update;
+
+public record Request(ProductDto Product);
+public record Response(bool IsSuccess);
+
+public class Endpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products/{id}", async (Guid id, Request request, ISender sender) =>
+        {
+            var command = request.Adapt<Command>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<Response>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<Response>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
---
+++ src/API/ESkitNet.API/Products/Update/Handler.cs
@@ -0,0 +1,37 @@
+﻿namespace ESkitNet.API.Products.Update;
+
+public record Command(ProductDto Product) : ICommand<Result>;
+
+public record Result(bool IsSuccess);
+
+public class Handler(StoreDbContext dbContext): ICommandHandler<Command, Result>
+{
+    public async Task<Result> Handle(Command command, CancellationToken cancellationToken)
+    {
+        //Update Order entity from command object
+        //save to database
+        //return result
+        var productId = ProductId.Of(command.Product.Id);
+        var product = await dbContext.Products
+            .FindAsync([productId], cancellationToken: cancellationToken);
+
+        if (product is null)
+            throw new ProductNotFoundException(command.Product.Id);
+
+        //order.UpdateFromDto(command.Order);
+
+        product.Name = command.Product.Name;
+        product.Description = command.Product.Description;
+        product.Price = command.Product.Price;
+        product.PictureUrl = command.Product.PictureUrl;
+        product.Type = command.Product.Type;
+        product.ProductBrand = command.Product.ProductBrand;
+        product.QuantityInStock = command.Product.QuantityInStock;
+
+        dbContext.Products.Update(product);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new Result(true);
+    }
+}
