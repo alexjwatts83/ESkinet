c5f6a9d7193203f8d6172146580adf92dda26eb7
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-26 21:25:26 +1100

    some changes to the front end but want to make additional changes to the backend first

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./layout/header/header.component";
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { Product } from './shared/models/products';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private httpClient = inject(HttpClient);
+
   title = 'client';
+  baseUrl = 'https://localhost:5151';
+  products: Product[] = [];
+
+  constructor() {
+    console.log('here');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.httpClient.get<Pagination<Product>>(`${this.baseUrl}/products?pageNumber=1&pageSize=10`).subscribe({
+      next: x => {
+        this.products = x.data;
+        console.log({prod: this.products});
+      },
+      error: error => console.error(error),
+      complete: () => console.log('completed')
+    });
+  }
 }
---
+++ src/client/src/app/shared/models/products.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: Number;
+  name: string;
+  description: string;
+  price: Number;
+  pictureUrl: string;
+  type: string;
+  brand: string;
+  quantityInStock: Number;
+};
