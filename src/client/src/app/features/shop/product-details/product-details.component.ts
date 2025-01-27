b24e8169c7a30b6ccbbfc3d6acb5d70be54aa87d
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-27 23:49:26 +1100

    added checkout component but does nothing

--- src/client/src/app/features/shop/product-details/product-details.component.ts
+++ src/client/src/app/features/shop/product-details/product-details.component.ts
@@ -2,25 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/products';
-import { CurrencyPipe, JsonPipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [JsonPipe, CurrencyPipe, MatIcon, MatButton, MatFormField, MatInput, MatLabel, MatDivider],
+  imports: [
+    CurrencyPipe,
+    MatIcon,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider,
+    FormsModule,
+  ],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService);
   private activatedRoute = inject(ActivatedRoute);
+  private cartService = inject(CartService);
 
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
 
   ngOnInit(): void {
     this.loadProduct();
@@ -28,16 +42,41 @@ export class ProductDetailsComponent implements OnInit {
 
   private loadProduct() {
     const productId = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log({productId});
-    if (!productId)
-      return;
+    console.log({ productId });
+    if (!productId) return;
 
     this.shopService.getProduct(productId).subscribe({
-      next: product => {
+      next: (product) => {
+        console.log({ product });
         this.product = product;
-        console.log({product});
+        this.updateQtyInCart();
       },
-      error: error => console.error(error)
-    })
+      error: (error) => console.error(error),
+    });
+  }
+
+  updateQtyInCart() {
+    this.quantityInCart =
+      this.cartService
+        .cart()
+        ?.items.find((x) => x.productId == this.product?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update cart' : 'Add to cart';
+  }
+
+  updateCart() {
+    if (!this.product) return;
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
   }
 }
