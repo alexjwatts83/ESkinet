fe54f9971b2fe2c1fe9bb51b83dc8180ee401518
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-31 14:06:08 +1100

    order details mostly done

--- src/client/src/app/features/orders/order-details/order-details.component.ts
+++ src/client/src/app/features/orders/order-details/order-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 import { OrderService } from '../../../core/services/order.service';
 import { map, shareReplay, switchMap } from 'rxjs';
 import { AsyncPipe, CurrencyPipe, DatePipe, JsonPipe, NgIf } from '@angular/common';
@@ -11,7 +11,7 @@ import { AddressPipe } from '../../../shared/pipes/address.pipe';
 @Component({
   selector: 'app-order-details',
   standalone: true,
-  imports: [NgIf, AsyncPipe, JsonPipe, MatCardModule, MatButton, DatePipe, CurrencyPipe, AddressPipe, CardPipe],
+  imports: [NgIf, AsyncPipe, JsonPipe, MatCardModule, MatButton, DatePipe, CurrencyPipe, AddressPipe, CardPipe, RouterLink],
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.scss',
 })
