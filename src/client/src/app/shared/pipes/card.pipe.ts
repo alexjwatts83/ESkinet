d8900f532dc620e5fea1f6832d101931b0b5d385
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-01-31 13:55:47 +1100

    update card pipe

--- src/client/src/app/shared/pipes/card.pipe.ts
+++ src/client/src/app/shared/pipes/card.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { PaymentSummary } from '../models/orders.models';
 
 @Pipe({
   name: 'card',
@@ -7,13 +8,19 @@ import { ConfirmationToken } from '@stripe/stripe-js';
 })
 export class CardPipe implements PipeTransform {
   transform(
-    value?: ConfirmationToken['payment_method_preview'],
+    value?: ConfirmationToken['payment_method_preview'] | PaymentSummary,
     ...args: unknown[]
   ): unknown {
-    if (value?.card) {
-      const { brand, last4, exp_month, exp_year } = value.card;
+    if (value && 'card' in value) {
+      const { brand, last4, exp_month, exp_year } = (value as ConfirmationToken['payment_method_preview'])?.card!;
       return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${exp_month}/${exp_year}`;
     }
+
+    if (value && 'expMonth' in value) {
+      const { brand, last4, expMonth, expYear } = value as PaymentSummary;
+      return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${expMonth}/${expYear}`;
+    }
+    
     return 'Unknown Card';
   }
 }
