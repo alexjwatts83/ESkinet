fc6e037ecf5b49d23d3d7b5c92f9a27dfefdf786
WIN-F7470BLT8P0\kamrui-pc <alexjwatts83@gmail.com>
2025-02-02 18:15:52 +1100

    changed fallback index.html location

--- src/API/ESkitNet.API/Fallback/FallbackController.cs
+++ src/API/ESkitNet.API/Fallback/FallbackController.cs
@@ -6,7 +6,7 @@ public class FallbackController(ILogger<FallbackController> logger) : Controller
 {
     public IActionResult Index()
     {
-        var indexFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+        var indexFile = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
         logger.LogInformation("{IndexFile} exists {Exists}", indexFile, System.IO.File.Exists(indexFile));
         return PhysicalFile(indexFile, "text/HTML");
     }
